39a3cc921abc2e3f3c4b6f7333c0b051e9e218b6
Barta Máté <m.barta@falatozz.hu>
2023-01-17 10:34:19 +0100

    Change for diff practice added

--- falatozzAppMate/resources/views/home.blade.php
+++ falatozzAppMate/resources/views/home.blade.php
@@ -25,6 +25,7 @@
     <div>
         <h2>My Larevel Project For Github Practice.</h2>
         <p>All credits belongs to Falatozz.hu</p>
+        <p>Kiegészítés</p>
     </div>
     </body>
 </html>
